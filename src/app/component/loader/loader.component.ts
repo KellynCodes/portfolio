839815870427374b6071d2d40a8bbcc0664b87bb
kellyncodes <kellyncodes@gmail.com>
2024-07-23 16:40:29 +0100

    added vortex project and new resume.

--- src/app/component/loader/loader.component.ts
+++ src/app/component/loader/loader.component.ts
@@ -1,13 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'blog-loader',
+  selector: 'kelly-loader',
   standalone: true,
   imports: [],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss',
 })
 export class LoaderComponent {
-  @Input({ required: true })
-  loadingText: string = '';
+  loadingText = input.required<string>();
 }
